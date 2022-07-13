e7f3578b8abb3d55a0310accbd10d392fc2054b1
Nadilson <nadilson@protonmail.com>
2022-07-13 03:26:17 -0300

    Documentação do Heading

---
+++ src/stories/Heading.stories.js
@@ -0,0 +1,29 @@
+import Heading from "./../components/Heading";
+
+export default {
+  title: "Heading",
+  component: Heading,
+};
+
+export const usage = () => (
+  <div>
+    <Heading>
+      <h1>Título 1</h1>
+    </Heading>
+    <Heading>
+      <h2>Título 2</h2>
+    </Heading>
+    <Heading>
+      <h3>Título 3</h3>
+    </Heading>
+    <Heading>
+      <h4>Título 4</h4>
+    </Heading>
+    <Heading>
+      <h5>Título 5</h5>
+    </Heading>
+    <Heading>
+      <h6>Título 6</h6>
+    </Heading>
+  </div>
+);
